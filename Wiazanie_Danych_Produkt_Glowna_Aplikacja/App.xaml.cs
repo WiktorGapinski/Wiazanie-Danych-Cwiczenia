b5ce232a850df3d43df8121b577e9169b6a8d0c4
Wiktor Gapiński <wiktor_gapinski@proton.me>
2024-12-08 21:37:28 +0100

    Dodanie plików aplikacji Produkt

---
+++ Wiazanie_Danych_Produkt_Glowna_Aplikacja/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Wiazanie_Danych_Produkt_Glowna_Aplikacja
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
