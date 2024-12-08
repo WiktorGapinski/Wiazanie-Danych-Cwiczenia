11915db58f83a8af582fe22b79c950f13ef62f02
Wiktor Gapiński <wiktor_gapinski@proton.me>
2024-12-08 21:53:28 +0100

    Modyfikacja code-behind w MainWindow: Dodanie obiektu Produkt, kontekstu danych i obsługi kliknięcia przycisku Potwierdź

--- Wiazanie_Danych_Produkt_Glowna_Aplikacja/MainWindow.xaml.cs
+++ Wiazanie_Danych_Produkt_Glowna_Aplikacja/MainWindow.xaml.cs
@@ -1,24 +1,27 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Wiazanie_Danych_Produkt_Glowna_Aplikacja
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Produkt pl = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            PrzygotujWiazanie();
+        }
+
+        private void PrzygotujWiazanie()
+        {
+            pl = new Produkt("DZ-10", "długopis żelowy", 132, "Katowice 1");
+            gridProdukt.DataContext = pl;
+        }
+
+        private void btnPotwierdz_Click(object sender, RoutedEventArgs e)
+        {
+            string tekst = string.Format("{0} {1}{2}", "Wprowadzono dane:", Environment.NewLine, pl.ToString());
+            MessageBox.Show(tekst);
         }
     }
 }
