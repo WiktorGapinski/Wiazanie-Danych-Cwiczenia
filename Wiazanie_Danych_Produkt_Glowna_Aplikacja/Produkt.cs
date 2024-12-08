3e19b87863f861ef03f58747431a80ffaca95f74
Wiktor Gapiński <wiktor_gapinski@proton.me>
2024-12-08 21:52:35 +0100

    Dodanie klasy Produkt z właściwościami, konstruktorem i metodą ToString

---
+++ Wiazanie_Danych_Produkt_Glowna_Aplikacja/Produkt.cs
@@ -0,0 +1,23 @@
+﻿namespace Wiazanie_Danych_Produkt_Glowna_Aplikacja
+{
+    public class Produkt
+    {
+        public string Symbol { get; set; }
+        public string Nazwa { get; set; }
+        public int LiczbaSztuk { get; set; }
+        public string Magazyn { get; set; }
+
+        public Produkt(string sym, string naz, int lszt, string mag)
+        {
+            Symbol = sym;
+            Nazwa = naz;
+            LiczbaSztuk = lszt;
+            Magazyn = mag;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2} {3}", Symbol, Nazwa, LiczbaSztuk, Magazyn);
+        }
+    }
+}
